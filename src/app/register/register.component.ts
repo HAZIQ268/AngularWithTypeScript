7abb89aafb967b823fbded4c0794ebd2cfac0596
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-07-22 18:52:08 +0500

    Angular With Type Script

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+email :string = "";
+password :string = "";
+check :boolean = false;
+
+myfunc() :void {
+  if(this.email == "" || this.password == "" || this.check == false){
+    alert("please fill out all the fields !");
+
+  }
+  else{
+    alert("Your Form Has Been Submited");
+    
+  }
+}
+
+}
