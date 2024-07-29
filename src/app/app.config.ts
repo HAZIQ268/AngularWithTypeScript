1480e38f3e9b2a65f643cee7ccf5cc83c299c380
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-07-29 18:30:43 +0500

    next page method

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, Component } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { RegisterComponent } from './register/register.component';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { FakeApiDataComponent } from './fake-api-data/fake-api-data.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter([
+    { path : '',redirectTo :'register',pathMatch:'full'},
+    { path : 'register',component:RegisterComponent},
+    { path : 'fakeApiData',component:FakeApiDataComponent},
+  ]), provideClientHydration()]
 };
